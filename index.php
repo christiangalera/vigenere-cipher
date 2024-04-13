e46237aef1339c12555ba4630b2b883614d68fb0
Christian <chrisgalera1555@gmail.com>
2024-04-13 01:52:26 -0300

    decrypt function

--- index.php
+++ index.php
@@ -18,5 +18,31 @@ function vigenere_encrypt($text, $key) {
   return $cipherText;
 }
 
-echo vigenere_encrypt('hello world', 'php');
+function vigenere_decrypt($cipherText, $key) {
+  $keyLength = strlen($key);
+  $cipherText = strtoupper($cipherText);
+  $key = strtoupper($key);
+  $text = '';
+
+  for ($i = 0; $i < strlen($cipherText); $i++) {
+    $char = $cipherText[$i];
+    if(ctype_alpha($char)) {
+      $offset = ord($key[$i % $keyLength]) - 65;
+      $text .= chr(((ord($char) - 65 - $offset + 26) % 26) + 65);
+    } else {
+      $text .= $char;
+    }
+  }
+  return $text;
+}
+
+const key = 'php';
+const message = 'hello world';
+
+$encryptedMessage = vigenere_encrypt(message, key);
+
+$decryptedMessage = vigenere_decrypt($encryptedMessage, key);
+echo "encrypted message: $encryptedMessage
+decrypted message: $decryptedMessage \n";
+
 
